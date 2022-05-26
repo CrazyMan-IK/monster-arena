706a3d9c3066a20e92afcb456111fe17d273a03d
Даниэль Давлетшин <Superdanik2004@gmail.com>
2022-05-26 14:07:30 +0500

    Second level color correction

--- Assets/Scripts/CustomPostProcess.cs
+++ Assets/Scripts/CustomPostProcess.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 namespace MonsterArena
 {
+    [ExecuteAlways]
     public class CustomPostProcess : MonoBehaviour
     {
         [SerializeField] private Material _material = null;
 
         private void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
+            if (_material == null)
+            {
+                return;
+            }
+            
             Graphics.Blit(source, destination, _material);
         }
     }
